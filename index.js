df57a0ff76ec2c7c991a71656874287deca139d9
Totan Sau <totansau7@gmail.com>
2024-11-18 22:56:03 +0530

    changed dot env file

--- index.js
+++ index.js
@@ -17,8 +17,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 // Api's
